d4fb3510e8e879d2bd77c2260be1e9358663667d
olaolu007 <desireosunde07@gmail.com>
2026-06-17 20:49:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -150,18 +150,20 @@ def main():
     
     import telegram
     print("Telegram version:", telegram.__version__)
+    
     class HealthHandler(BaseHTTPRequestHandler):
-      def do_GET(self):
-        self.send_response(200)
-        self.end_handlers()
-        self.wfile.write(b"OK")
-      def log_message(self, format, *args):
-        pass
+          def do_GET(self):
+              self.send_response(200)
+              self.end_headers()
+              self.wfile.write(b"OK")
+          def log_message(self, format, *args):
+              pass
+
     def run_health_server():
-        server = HTTPServer(("0.0.0.0", 8000) HealthHandler)
+        server = HTTPServer(("0.0.0.0", 8080), HealthHandler)
         server.serve_forever()
 
-     threading.Thread(target=run_health_server, daemon = True).start()
+    threading.Thread(target=run_health_server, daemon = True).start()
     print("Bot is running...")
     
     app.run_polling()
